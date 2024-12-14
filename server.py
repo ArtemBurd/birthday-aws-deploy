0626da02e2f2407ff740b03179fde9ec807e3d75
Artem Burdeinyi <artem_burd@ukr.net>
2024-12-14 20:23:09 +0200

    Make CD with /when endpoint

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ def days_until_birthday(birthday_str):
 @app.route("/health", methods = ["GET"])
 def health_check():
     return "The light inside has broken but i still work", 200
-"""
+
 @app.route("/when", methods = ["POST"])
 def get_next_bday():
     birthday_str = request.get_json().get("birthday")
@@ -52,7 +52,7 @@ def get_next_friend_bday(id):
     days_left = days_until_birthday(birthday_str)
     friend["Days until next birthday"] = days_left
     return jsonify(friend), 200
-"""
+
 # READ all
 @app.route('/friends', methods=['GET'])
 def get_friends():
